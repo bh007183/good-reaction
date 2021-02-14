e916ae81eedebf6e8a32118584be7db1c1ca4537
Ben Hopkins <bjhops17@gmail.com>
2021-02-14 13:36:02 -0800

    getting closer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+
 import Employee from "./camponents/employee"
 
 function App() {
   return (
     <>
     <Employee/>
-  </>
+    </>
   );
 }
 
